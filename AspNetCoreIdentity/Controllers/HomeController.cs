52ec627bbcda693342b06fa20eaed408003cd42a
Bruno <brunocbarreto2012@gmail.com>
2021-02-12 09:04:42 -0300

    Testando o uso de claims customizadas

--- AspNetCoreIdentity/Controllers/HomeController.cs
+++ AspNetCoreIdentity/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using AspNetCoreIdentity.Models;
+﻿using AspNetCoreIdentity.Extensions;
+using AspNetCoreIdentity.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -37,6 +38,12 @@ namespace AspNetCoreIdentity.Controllers
 
         [Authorize(Policy = "PodeGravar")]
         public IActionResult SecretClaimGravar()
+        {
+            return View("Secret");
+        }
+
+        [ClaimsAuthorize("Produto", "Ler")]
+        public IActionResult ClaimsCustom()
         {
             return View("Secret");
         }
