efde813ea55d904040c8db03257273f1b4ce88bb
Bruno Barreto <brunocbarreto2012@gmail.com>
2021-02-15 22:41:54 -0300

    Testando ambientes diferentes de execução

--- src/AspNetCoreIdentity/obj/Debug/netcoreapp2.2/AspNetCoreIdentity.AssemblyInfo.cs
+++ src/AspNetCoreIdentity/obj/Debug/netcoreapp2.2/AspNetCoreIdentity.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("3be7ff3e-121c-4692-90de-cfc7c87b1f53")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
